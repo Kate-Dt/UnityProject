669b90a1554efc5900c7a74a8d04977f3f3286ae
Kate-Dt <dotsiuk.k@gmail.com>
2018-06-11 22:25:41 +0200

    practice 3 added crystals

--- Assets/Scripts/Crystal.cs
+++ Assets/Scripts/Crystal.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Crystals : Collectable
+public class Crystal : Collectable
 {
     protected override void OnRabitHit(HeroRabbit rabit)
     {
